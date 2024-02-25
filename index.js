dde284f957eacc26aaff6b4037fca6d0555ed1f9
krishna <krishna@shravtek.com>
2024-02-25 21:23:41 +0530

    Initial Commit UniConverter Service API
    Server '/' only created
    helmet, cors added

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const helmet = require("helmet");
+const PORT = 3000;
+
+app.use(cors());
+app.use(helmet());
+
+app.get("/", (req, res) => {
+  res.write("OOPS This is a service API unit, Please Contact Admin");
+  res.end();
+});
+
+app.listen(PORT, (e) => {
+  if (e) {
+    console.error(e);
+    return;
+  }
+  console.log(`Server Up at port ${PORT}`);
+});

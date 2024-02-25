447e763d467a3941b0459e3192bd53c81639f2fd
krishna <krishna@shravtek.com>
2024-02-25 21:42:18 +0530

    Added vercel.json file

--- api/index.js
+++ api/index.js
@@ -8,6 +8,8 @@ app.use(cors());
 app.use(helmet());
 
 app.get("/", (req, res) => {
+  res.setHeader("Content-Type", "text/html");
+  res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
   res.write("OOPS This is a service API unit, Please Contact Admin");
   res.end();
 });
